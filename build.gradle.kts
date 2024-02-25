6d96b42f98879a23879c4bc8507303a7c536729e
Daniel Espendiller <daniel@espendiller.net>
2024-02-25 10:26:13 +0100

    update gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.9.21"
-    id("org.jetbrains.intellij") version "1.16.1"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+    id("org.jetbrains.intellij") version "1.17.2"
 }
 
 group = "de.espend.idea.vuejs"
