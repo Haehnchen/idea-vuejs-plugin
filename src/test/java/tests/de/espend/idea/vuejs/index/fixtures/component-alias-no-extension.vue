f123141c8bf563fe527a19970a619ce3299abc44
Daniel Espendiller <daniel@espendiller.net>
2024-02-25 17:13:34 +0100

    add more in the wild test file

---
+++ src/test/java/tests/de/espend/idea/vuejs/index/fixtures/component-alias-no-extension.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <dynamic-renderer></dynamic-renderer>
+      <static-renderer></static-renderer>
+    </div>
+</template>
+
+<script>
+import dynamicRenderer from '~/components/dynamic-renderer';
+import staticRenderer from '~/components/static-renderer';
+
+export default {
+  components: {
+    'dynamic-renderer': dynamicRenderer,
+    'static-renderer': staticRenderer
+  }
+}
+</script>
